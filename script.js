79fe3f40b3cc9ecb2c7a79375c75129ff35a4212
Septian Jordan Purba <142944749+sjordanp12@users.noreply.github.com>
2023-11-07 21:35:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top = '0';
+  } else {
+    document.getElementById('navbar').style.top = '-50px';
+  }
+  prevScrollpos = currentScrollPos;
+};
